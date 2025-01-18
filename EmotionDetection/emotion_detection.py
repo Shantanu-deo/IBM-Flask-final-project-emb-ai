669a3fb32c11f1625efcd534e83cf3cdb6b1c033
Shantanu-deo <shantanu.deo@gmail.com>
2025-01-18 12:29:10 +0000

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,11 +1,6 @@
-from flask import Flask,request
-import requests
 import json
-app = Flask(__name__)
-
-
-
-def emotion_detector(text_to_analyze):
+import requests
+def emotion_detector(text):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock",
@@ -13,19 +8,17 @@ def emotion_detector(text_to_analyze):
     }
     input_json = {
         "raw_document": {
-            "text": text_to_analyze
+            "text": text
         }
     }
     response = requests.post(url, headers=headers, json=input_json)
     if response.status_code == 200:
         return response.json()['emotionPredictions'][0]['emotion']
+    elif response.status_code == 400:
+        invalidEmotionDict=  { "anger": None,"disgust": None,
+        "fear": None,"joy": None,"sadness": None}
+        return invalidEmotionDict
     else:
         return f"Error: {response.status_code}, {response.text}"
 
-@app.route('/get_emotion')
-def get_emotion():
-    text_to_analyze =  request.args.get('text')
-    return emotion_detector(text_to_analyze)
 
-if __name__ == '__main__':
-    app.run(debug=True)
